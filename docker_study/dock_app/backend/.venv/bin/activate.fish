8167e323f1e468deeded57764eaf748afd658cb9
soumyadeep74 <soumyadeepsen74@gmail.com>
2025-06-06 23:33:21 +0530

    add

--- docker_study/dock_app/backend/.venv/bin/activate.fish
+++ docker_study/dock_app/backend/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/soumya/Desktop/docker_study/dock_app/backend/.venv
+set -gx VIRTUAL_ENV /home/soumya/Documents/TUTEDUDE/docker_study/dock_app/backend/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
