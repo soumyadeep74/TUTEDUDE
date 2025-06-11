4812ead0c862731a2ac17b0eba3989ead332e9ba
soumyadeep74 <soumyadeepsen74@gmail.com>
2025-06-11 19:09:10 +0530

    files

--- Tutedude_Assignment/DOCKER_Assignment/frontend/app.js
+++ Tutedude_Assignment/DOCKER_Assignment/frontend/app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'templates')));
 
-app.post('/submit', (req, res) => {
+//app.post('/submit', (req, res) => {
     // Redirect the POST request to the Flask backend
-    res.redirect(307, 'http://backend:5000/submit');
-});
+   // res.redirect(307, 'http://backend:5000/submit');
+//});
 
 app.listen(port, () => {
     console.log(`Frontend running at http://localhost:${port}`);
