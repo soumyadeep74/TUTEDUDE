f93b78900b1bade09bcd446d81f85e487c0644ff
soumyadeep74 <soumyadeepsen74@gmail.com>
2025-06-13 16:45:59 +0530

    files

--- AWS/boto3-study/app.py
+++ AWS/boto3-study/app.py
@@ -1,7 +1,8 @@
 import boto3
-
+ACCESS_KEY_ID='AKIAUP5E6PV6FQ6KLLGC'
+SECRET_ACCESS_KEY='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'
 #create a s3 client
-s3 = boto3.resource('s3')
+s3 = boto3.resource('s3', aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
 
 #upload a new file
 for bucket in s3.buckets.all():
