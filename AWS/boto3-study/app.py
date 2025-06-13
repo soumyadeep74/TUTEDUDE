5694f1fc62add03b5d95fad671d27e7f34eb3417
soumyadeep74 <soumyadeepsen74@gmail.com>
2025-06-13 16:55:51 +0530

    files

--- AWS/boto3-study/app.py
+++ AWS/boto3-study/app.py
@@ -1,6 +1,11 @@
 import boto3
-ACCESS_KEY_ID='AKIAUP5E6PV6FQ6KLLGC'
-SECRET_ACCESS_KEY='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'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ACCESS_KEY_ID = os.getenv("ACCESS_KEY_ID")
+SECRET_ACCESS_KEY = os.getenv("SECRET_ACCESS_KEY")
 #create a s3 client
 s3 = boto3.resource('s3', aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
 
