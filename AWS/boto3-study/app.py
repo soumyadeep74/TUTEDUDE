937cba54926c51396c129be919d662450b7c5517
soumyadeep74 <soumyadeepsen74@gmail.com>
2025-06-13 17:07:52 +0530

    files

--- AWS/boto3-study/app.py
+++ AWS/boto3-study/app.py
@@ -9,6 +9,9 @@ SECRET_ACCESS_KEY = os.getenv("SECRET_ACCESS_KEY")
 #create a s3 client
 s3 = boto3.resource('s3', aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
 
+#BUCKET_NAME = 'course-bucket-2-tute'
+#s3.create_bucket(Bucket=BUCKET_NAME, CreateBucketConfiguration={'LocationConstraint': 'ap-south-1'})
 #upload a new file
 for bucket in s3.buckets.all():
-    print(bucket.name)
+    location = s3.meta.client.get_bucket_location(Bucket=bucket.name)['LocationConstraint']
+    print(bucket.name, location)
